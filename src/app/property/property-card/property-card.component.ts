1786e5ccef9652431ec3deb2990fb266558ac09e
MirnaMilad <mirnamilad0101@gmail.com>
2025-04-14 18:12:24 +0200

    add property card and nav-bar components with styling; remove unused card-property component

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "app-property-card",
+  standalone: true,
+  imports: [],
+  templateUrl: "./property-card.component.html",
+  styleUrl: "./property-card.component.scss",
+})
+export class PropertyCardComponent {
+  @Input() property: any;
+}
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { PropertyCardComponent } from "../property-card/property-card.component";
 
 @Component({
-  selector: 'app-property-list',
+  selector: "app-property-list",
   standalone: true,
-  imports: [],
-  templateUrl: './property-list.component.html',
-  styleUrl: './property-list.component.scss'
+  imports: [PropertyCardComponent],
+  templateUrl: "./property-list.component.html",
+  styleUrl: "./property-list.component.scss",
 })
 export class PropertyListComponent {
-
+  properties: Array<any> = [
+    {
+      id: 1,
+      name: "Beautiful House",
+      type: "House",
+      price: 500000,
+    },
+    {
+      id: 2,
+      name: "Luxury Apartment",
+      type: "Apartment",
+      price: 750000,
+    },
+    {
+      id: 3,
+      name: "Cozy Cottage",
+      type: "Cottage",
+      price: 300000,
+    },
+    {
+      id: 4,
+      name: "Modern Villa",
+      type: "Villa",
+      price: 900000,
+    },
+    {
+      id: 5,
+      name: "Charming Bungalow",
+      type: "Bungalow",
+      price: 400000,
+    },
+    {
+      id: 6,
+      name: "Spacious Townhouse",
+      type: "Townhouse",
+      price: 600000,
+    },
+  ];
 }

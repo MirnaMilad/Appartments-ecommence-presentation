c93107390717d2a7bf254ce1af9221449b52ca5b
MirnaMilad <mirnamilad0101@gmail.com>
2025-04-14 15:27:22 +0200

    add nav-bar, card-property, and property-list components with basic templates and styles

---
+++ src/app/property/card-property/card-property.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-property',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-property.component.html',
+  styleUrl: './card-property.component.scss'
+})
+export class CardPropertyComponent {
+
+}
